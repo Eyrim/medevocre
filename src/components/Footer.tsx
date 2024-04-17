0b8649ef0767a7d4d4388680ffe7d1fcd41c7b8b
Eyrim <marfxedbot@gmail.com>
2024-04-17 21:19:43 +0100

    Import octocat logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import '../css/Footer.css'
 import '../scripts/load-cat'
+import octocatLogo from '../assets/github-mark-white.svg'
 
 function App() {
     return (
         <footer>
             <a id="octocat-carrier" target="_blank" href="https://github.com/Eyrim">
-                <img src="assets/github-mark-white.svg" alt="" />
+                <img src={octocatLogo} alt="" />
             </a>
             <div id="cat-carrier"></div>
         </footer>
