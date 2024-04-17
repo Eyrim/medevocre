42ed0a742b1bf9f59b65f596db193d9a86ed09c7
Eyrim <marfxedbot@gmail.com>
2024-04-17 19:54:05 +0100

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import '../css/Footer.css'
+import '../scripts/load-cat'
+
+function App() {
+    return (
+        <footer>
+            <a id="octocat-carrier" target="_blank" href="https://github.com/Eyrim">
+                <img src="/github-mark-white.svg" alt="" />
+            </a>
+            <div id="cat-carrier"></div>
+        </footer>
+    )
+}
+
+export default App
